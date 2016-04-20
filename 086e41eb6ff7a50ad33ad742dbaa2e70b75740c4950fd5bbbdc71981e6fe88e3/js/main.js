c88b5ee3074208d51a8d0601b74238978a5dc450
Bernhard <bernhard.gruenewaldt@comsysto.com>
2016-04-20 17:31:24 +0200

    dummy page

---
+++ 086e41eb6ff7a50ad33ad742dbaa2e70b75740c4950fd5bbbdc71981e6fe88e3/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    var box = document.getElementById("cs-box-green");
+    box.style.color = "#fff";
+    box.style['background-color'] = "#669933";
+
+});
